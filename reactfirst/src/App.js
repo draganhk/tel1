b34bf4c3043ece05d64f8d5a4e706caed6005b2d
Dragan <dragan.hadji-kotarov@musala.com>
2022-06-03 18:34:32 +0200

    tel1 - ReDone #Made it cleaner and minimized number of used vars

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -6,15 +6,15 @@ class App extends React.Component {
     super(props);
     this.state = {
       fullname: '',
+      fullnameVal: '' ,
       email: '',
-      valid: true,
+      emailVal: true,
       flexible: false ,
       days: '',
-      message: false
+      daysVal: '' ,
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleEmail = this.handleEmail.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -30,58 +30,58 @@ class App extends React.Component {
     // console.log(name+":"+value);
   }
 
-  //email validation
-  handleEmail(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    // if (value.length>6){
-      if (typeof value !== "undefined") {
-          var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        
-          if (!pattern.test(value)) {
-            this.setState({
-              valid: false
-            });
-          }else{
-            this.setState({
-              valid: true
-            });
-          }
-      }
-    // }
-
-    this.setState({
-      [name]: value
-    });
-
-    // console.log(name+":"+value+"  valid:"+this.state.valid);
-  }
-
   handleSubmit(event) {
     event.preventDefault();
-    if (!this.state.flexible && !isFinite(this.state.days)){
+
+    //fullname validation
+    const nameValue = this.state.fullname;
+    if (nameValue.length){
       this.setState({
-        message: true
+        fullnameVal: nameValue.length>100 ? "Your Full Name is over 100 characters" : ""
       });
-    }else{
+    } else{
       this.setState({
-        message: false
+        fullnameVal: "THIS FIELD IS REQUIRED"
       });
     }
-    // console.log("flexible: "+this.state.flexible+ " | days:"+this.state.days);
+
+
+    //email validation
+    const emailValue = this.state.email;
+    if (emailValue.length){
+      if (typeof emailValue !== "undefined") {
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        this.setState({
+          emailVal: !pattern.test(emailValue) ? "Please enter valid email address" : ""
+        });
+      }
+    }
+
+    //if not flexible -> numeric validation
+    const flexibleValue = this.state.flexible;
+    const daysValue = this.state.days;
+    if (!flexibleValue){
+      if (daysValue.length===0){
+        this.setState({
+          daysVal:  "THIS FIELD IS REQUIRED"
+        });
+      } else{
+        this.setState({
+          daysVal: !isFinite(daysValue) ? "Your days of availability are not specified!" : ""
+        });
+      }
+    }else{
+      this.setState({
+        daysVal: ""
+      });
+    }
   }
 
   render() {
-    const isCheckedIn = this.state.flexible;
-    const isValidEmail = this.state.valid;
-    const isValidDays = this.state.message;
-
-    let star, valid, err;
-    star = isCheckedIn ? "" : "*";
-    valid = isValidEmail ? "" : "Please enter valid email address.";
-    err = isValidDays ? "Your days of availability are not specified!" : "";
+    const strFullNameValid = this.state.fullnameVal;
+    const strEmailValid = this.state.emailVal;
+    const bIsCheckedIn = this.state.flexible;
+    const strDaysValid = this.state.daysVal;
 
     return (
       <div className="App">
@@ -89,28 +89,30 @@ class App extends React.Component {
           <div className="Text-red">
             * Required Fields
           </div>
+          <br></br>
           <div>
             Full Name* :&nbsp;
             <input type="text" name="fullname" value={this.state.fullname}
-            onChange={this.handleChange} maxLength="100" required></input>
+            onChange={this.handleChange} required></input>
+            &nbsp;<span className="Text-red" name="fullname-valid">{strFullNameValid}</span>
           </div>
           <div>
-            Email Address:&nbsp;
+            Email Address :&nbsp;
             <input type="text" name="email" value={this.state.email}
-            onChange={this.handleEmail}></input>
-            &nbsp;<span className="Text-red" name="valid">{valid}</span>
+            onChange={this.handleChange}></input>
+            &nbsp;<span className="Text-red" name="email-valid">{strEmailValid}</span>
           </div>
           <div>
-            Flexible:&nbsp;
+            Flexible :&nbsp;
             <input type="checkbox" name="flexible" value={this.state.flexible}
             onChange={this.handleChange}></input>
           </div>
           <div>
-            Days of Availability {star}:&nbsp;
+            Days of Availability {bIsCheckedIn ? "" : "*"}:&nbsp;
             <input name="days" value={this.state.days}
-            onChange={this.handleChange} required={star}></input>
+            onChange={this.handleChange} required={bIsCheckedIn}></input>
+            &nbsp;<span className="Text-red" name="days-valid">{strDaysValid}</span>
           </div>
-          &nbsp;<span className="Text-red" name="err">{err}</span>
           <div>
             <button type="submit" onClick={this.handleSubmit}>Save</button>
           </div>
