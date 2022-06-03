4d2e30bc112fbec85155d8feb8b53da7651da244
Dragan <dragan.hadji-kotarov@musala.com>
2022-06-03 16:09:35 +0200

    tel1 - part 1 Done

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -7,12 +7,15 @@ class App extends React.Component {
     this.state = {
       fullname: '',
       email: '',
-      flexible: '',
+      valid: true,
+      flexible: false ,
       days: '',
+      message: false
     };
 
     this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -24,15 +27,62 @@ class App extends React.Component {
       [name]: value
     });
 
-    console.log(name+":"+value);
+    // console.log(name+":"+value);
   }
 
-  // handleSubmit(event) {
-  //   alert('Your favorite flavor is: ' + this.state.value);
-  //   event.preventDefault();
-  // }
+  //email validation
+  handleEmail(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    // if (value.length>6){
+      if (typeof value !== "undefined") {
+          var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        
+          if (!pattern.test(value)) {
+            this.setState({
+              valid: false
+            });
+          }else{
+            this.setState({
+              valid: true
+            });
+          }
+      }
+    // }
+
+    this.setState({
+      [name]: value
+    });
+
+    // console.log(name+":"+value+"  valid:"+this.state.valid);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.flexible && !isFinite(this.state.days)){
+      this.setState({
+        message: true
+      });
+    }else{
+      this.setState({
+        message: false
+      });
+    }
+    // console.log("flexible: "+this.state.flexible+ " | days:"+this.state.days);
+  }
 
   render() {
+    const isCheckedIn = this.state.flexible;
+    const isValidEmail = this.state.valid;
+    const isValidDays = this.state.message;
+
+    let star, valid, err;
+    star = isCheckedIn ? "*" : "";
+    valid = isValidEmail ? "" : "Please enter valid email address.";
+    err = isValidDays ? "Your days of availability are not specified!" : "";
+
     return (
       <div className="App">
       <header className="App-header">
@@ -40,27 +90,29 @@ class App extends React.Component {
             * Required Fields
           </div>
           <div>
-            Full Name* :
+            Full Name* :&nbsp;
             <input type="text" name="fullname" value={this.state.fullname}
-            onChange={this.handleChange} maxLength="100"></input>
+            onChange={this.handleChange} maxLength="100" required></input>
           </div>
           <div>
-            Email Address:
+            Email Address:&nbsp;
             <input type="text" name="email" value={this.state.email}
-            onChange={this.handleChange}></input>
+            onChange={this.handleEmail}></input>
+            &nbsp;<span className="Text-red" name="valid">{valid}</span>
           </div>
           <div>
-            Flexible:
+            Flexible:&nbsp;
             <input type="checkbox" name="flexible" value={this.state.flexible}
             onChange={this.handleChange}></input>
           </div>
           <div>
-            Days of Availability:
-            <input type="number" name="days" value={this.state.days}
-            onChange={this.handleChange}></input>
+            Days of Availability {star}:&nbsp;
+            <input name="days" value={this.state.days}
+            onChange={this.handleChange} required={star}></input>
           </div>
+          &nbsp;<span className="Text-red" name="err">{err}</span>
           <div>
-            <button type="submit">Save</button>
+            <button type="submit" onClick={this.handleSubmit}>Save</button>
           </div>
       </header>
     </div>
