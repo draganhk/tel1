d99af0d0961945d0e392f190459798d6f161f87a
Dragan <dragan.hadji-kotarov@musala.com>
2022-06-06 08:42:44 +0200

    tel1 - Initial cypress setup

---
+++ cypressthird/cypress/e2e/cypress-testing/tests.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('tel 1', () => {
+  
+  it("test 1", () => {
+    cy.forceVisit('http://localhost:3000/')
+  })
+})
---
+++ cypressthird/test/test.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('Tel1', () => {
+    it("test 1", () => {
+        cy.visit('http://localhost:3000/')
+    })
+})
+
+describe(|"Tel1", () => {
