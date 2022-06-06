9aae995e38fe62d1aa58825b6a06c4e6503af2b0
Dragan <dragan.hadji-kotarov@musala.com>
2022-06-06 15:00:32 +0200

    tel1 - Jest done

--- reactfirst/src/App.test.js
+++ reactfirst/src/App.test.js
@@ -1,20 +1,47 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('test 1', () => {
   render(<App />);
 
-  //select the elements you want to interact with
-  const inputFullname = screen.getByTestId('fullname');
+  //select the elements for interaction
+  const inputEmail = screen.getByTestId('email');
+  const spanEmailValid = screen.getByTestId('email-valid');
   const buttonSave = screen.getByTestId('save');
-  const spanFullnameValid = screen.getByTestId('fullname-valid');
-
 
   //interact with those elements
-  fireEvent.click(inputFullname);
-  inputFullname.insertAdjacentText("afterbegin", "test");
+  fireEvent.click(inputEmail);
+  fireEvent.change(inputEmail, {target: {value: 'dragan'}})
   fireEvent.click(buttonSave);
-// console.log(spanFullnameValid);
-  // const linkElement = screen.getByText(/learn react/i);
-  expect(spanFullnameValid).toBeInTheDocument();
+
+  var text = spanEmailValid.textContent;
+  //test validation
+  expect("Please enter valid email address").toBe(text);
 });
+
+
+test('test 2', () => {
+  render(<App />);
+
+  //select the elements for interaction
+  const checkboxFixed = screen.getByTestId('flexible');
+  const inputDays = screen.getByTestId('days');
+  const spanDaysValid = screen.getByTestId('days-valid');
+  const buttonSave = screen.getByTestId('save');
+
+  //interact with those elements
+  fireEvent.click(inputDays);
+  fireEvent.change(inputDays, {target: {value: 'dragan'}})
+  fireEvent.click(buttonSave);
+
+  //test validation
+  expect("Your days of availability are not specified!").toBe(spanDaysValid.textContent);
+
+  fireEvent.click(inputDays);
+  fireEvent.change(inputDays, {target: {value: '66'}})
+  fireEvent.click(buttonSave);
+
+  //test ReValidation
+  expect("").toBe(spanDaysValid.textContent);
+});
+
